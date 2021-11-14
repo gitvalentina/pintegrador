a0795c50dfdb680598506d1f0a5831a490df2d41
gitvalentina <vdouce@udesa.edu.ar>
2021-11-14 09:24:35 -0300

    Js para inicio + cambios de etiquetas semanticas

---
+++ js/home.js
@@ -0,0 +1,78 @@
+window.addEventListener('load', function(){
+
+fetch("https://api.themoviedb.org/3/movie/popular?api_key=184353d8f8f15b5e8908b2560e49a9f3")
+ .then(function(response){
+    return response.json();
+ })
+
+ .then(function(datos){
+     
+    console.log(datos.results);
+
+     for (let i = 0; i < 6; i++) {
+         document.querySelector('.pelis-populares').innerHTML += ` 
+         <article>
+            <a href="detalle-peliculas.html?id=${datos.results[i].id}" class="items-main"><img class="imghome"
+            src="https://image.tmdb.org/t/p/w342${datos.results[i].poster_path}" alt="big hero">
+            </a>
+            <p>${datos.results[i].title}</p>
+            <p>${datos.results[i].popularity} </p>
+         </article> 
+         `;
+        }
+     })
+ .catch(function(error){
+    console.log(`El error fue: ${error}`);
+ })
+})
+
+fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=184353d8f8f15b5e8908b2560e49a9f3")
+ .then(function(response){
+    return response.json();
+ })
+
+ .then(function(datos){
+     
+    console.log(datos.results);
+
+     for (let i = 0; i < 6; i++) {
+         document.querySelector('.mas-visto').innerHTML += `
+         <article>
+            <a href="detalle-peliculas.html?id=${datos.results[i].id}" class="items-main"><img class="imghome"
+               src="https://image.tmdb.org/t/p/w342${datos.results[i].poster_path}" alt="big hero">
+            </a>
+            <p>${datos.results[i].title}</p>
+            <p>${datos.results[i].vote_average} </p>
+         </article> 
+         `;
+        }
+     })
+ .catch(function(error){
+    console.log(`El error fue: ${error}`);
+})
+
+
+fetch("https://api.themoviedb.org/3/tv/popular?api_key=184353d8f8f15b5e8908b2560e49a9f3")
+ .then(function(response){
+    return response.json();
+ })
+
+ .then(function(datos){
+     
+    console.log(datos.results);
+
+     for (let i = 0; i < 6; i++) {
+         document.querySelector('.series-populares').innerHTML += `
+         <article>
+            <a href="detalle-peliculas.html?id=${datos.results[i].id}" class="items-main"><img class="imghome"
+            src="https://image.tmdb.org/t/p/w342${datos.results[i].poster_path}" alt="big hero">
+            </a>
+            <p>${datos.results[i].name}</p>
+            <p>${datos.results[i].popularity} </p>
+         </article> 
+         `;
+        }
+     })
+ .catch(function(error){
+    console.log(`El error fue: ${error}`);
+ })
---
+++ js/resultados.js
@@ -0,0 +1,7 @@
+// evento que controla que todo el html esté cargado en el navegador //
+window.addEventListener('load', function () {
+   
+    
+})
+
+
