849b0f04a4f362e7a05c9353f0aec26d4b06cf64
Nour Awada <nourawada@MacBook-Pro-de-Nour.local>
2021-11-20 10:37:01 -0300

    agrego boton de quitar y agregar a fav en detalles

--- js/detalle-peliculas.js
+++ js/detalle-peliculas.js
@@ -4,6 +4,7 @@ window.addEventListener("load" , function(){
     let movie_id = queryStringObj.get('id');
 
     let movies = document.querySelector('.mainclase')
+    let listaFavoritos = []
 
     fetch(`https://api.themoviedb.org/3/movie/${movie_id}?api_key=e815c822e45566d0a81a08ab74a27687`)
     .then(function(response){
@@ -32,18 +33,58 @@ window.addEventListener("load" , function(){
         <li>Duracion: ${runtimeMovie}</li>
         <li>Rating: ${ratingMovie}</li>
         <a href="./generos.html?id=${idMovie}"><li>Generos: ${genreMovie}</li></a>
-        <a href="./fav.html?id=${idMovie}"><li>Agregar a Favoritos</li></a>
+        <button class= "fav"> Agregar a Favoritos</button>
         </ul>
         </section>`
 
-    })
-    .catch(function (error) {
-        console.log(error)
-    })
-    
+            //Recuperso datos del storage
+//set item agrega una propiedad y sus valores a obj literal
+//para ver si habia algo
+let recuperoStorage= localStorage.getItem('fav');
+console.log(recuperoStorage)
+//en el caso de que haya elementos en storage. Osea no sea nulo,
+if (recuperoStorage != null){
+    //transformo el string en array
+    //parse transforma a json en obj literal
+    listaFavoritos=JSON.parse(recuperoStorage)//Parse toma cadena de texto en JSON y lo transforma en objeto literal
+}
+// Me fijo si el id de la canción esta en la lista
+//si esta cambio el texto para sacar
+//includes servia para ver si está o no
+if (listaFavoritos.includes(movie_id)){
+document.querySelector(".fav").innerHTML=`<button>Agregar a Favoritos</button></a>`
+console.log(listaFavoritos);
+}
+//Agregar a favs
+let agregarAFav= document.querySelector('.fav');
+agregarAFav.addEventListener('click', function(){
+    //si esta en la lista
+    if (listaFavoritos.includes(movie_id)){
+        //lo localizo en array INDEXOF-->LOCALIZAR
+        let sacarID= listaFavoritos.indexOf(movie_id);
+        //y lo saco SPLICE-->SACAR
+        listaFavoritos.splice(sacarID, 1);
+        //Si ya lo saque --> cambio el texto de link
+        agregarAFav.innerHTML=`Agregar a Favoritos`
+        console.log(listaFavoritos)
+    }
+    //si no esta en mi lista
+   else {
+        //se agrega la canción actual
+        listaFavoritos.push(movie_id);
+        //si ya lo agregué-->cambio texto 
+        agregarAFav.innerHTML = `Quitar de Favoritos`
 
-   
-
-    
-
+    }
+    //guardo el array actualizado como string
+    let trackAStorage= JSON.stringify(listaFavoritos);
+    //Guardo el string en local storage
+    localStorage.setItem('fav', trackAStorage)
+    //chequeo*/
+    console.log(localStorage)
+})
+})
+.catch(function (error) {
+    console.log(error)
+})
 })
--- js/detalle-series.js
+++ js/detalle-series.js
@@ -1,11 +1,12 @@
 window.addEventListener("load" , function(){
     let queryString = location.search;
     let queryStringObj = new URLSearchParams(queryString);
-    let tv_id = queryStringObj.get('id');
+    let tvid = queryStringObj.get('id');
 
-    let series = document.querySelector('.mainclase')
+    let series = document.querySelector('.mainclase');
+    let listaFavoritos = [];
 
-    fetch(`https://api.themoviedb.org/3/tv/${tv_id}?api_key=e815c822e45566d0a81a08ab74a27687`)
+    fetch(`https://api.themoviedb.org/3/tv/${tvid}?api_key=e815c822e45566d0a81a08ab74a27687`)
     .then(function(response){
         return response.json()
     })
@@ -13,7 +14,7 @@ window.addEventListener("load" , function(){
         console.log(datos);
 
         let imagenSerie = datos.poster_path
-        let tituloSerie = datos.original_title
+        let tituloSerie = datos.original_name
         let ratingSerie = datos.vote_average
         let releaseSerie = datos.first_air_date
         let overviewSerie = datos.overview
@@ -30,18 +31,62 @@ window.addEventListener("load" , function(){
         <li>Fecha de Estreno: ${releaseSerie}</li>
         <li>Rating: ${ratingSerie}</li>
         <a href="./generos.html?id=${idSerie}"><li>Generos: ${genreSerie}</li></a>
-        <a href="./fav.html?id=${idSerie}"><li>Agregar a Favoritos</li></a>
+        <button class= "fav"> Agregar a Favoritos</button>
         </ul>
         </section>`
+     //Defino array para guardar lista de favoritos
 
-    })
-    .catch(function (error) {
-        console.log(error)
-    })
-    
 
-   
+//Recuperso datos del storage
+//set item agrega una propiedad y sus valores a obj literal
+//para ver si habia algo
+let recuperoStorage= localStorage.getItem('fav');
+console.log(recuperoStorage)
+//en el caso de que haya elementos en storage. Osea no sea nulo,
+if (recuperoStorage != null){
+    //transformo el string en array
+    //parse transforma a json en obj literal
+    listaFavoritos=JSON.parse(recuperoStorage)//Parse toma cadena de texto en JSON y lo transforma en objeto literal
+}
+// Me fijo si el id de la canción esta en la lista
+//si esta cambio el texto para sacar
+//includes servia para ver si está o no
+if (listaFavoritos.includes(tvid)){
+document.querySelector(".fav").innerHTML=`<button>Agregar a Favoritos</button></a>`
+console.log(listaFavoritos);
+}
+//Agregar a favs
+let agregarAFav= document.querySelector('.fav');
+agregarAFav.addEventListener('click', function(){
+    //si esta en la lista
+    if (listaFavoritos.includes(tvid)){
+        //lo localizo en array INDEXOF-->LOCALIZAR
+        let sacarID= listaFavoritos.indexOf(tvid);
+        //y lo saco SPLICE-->SACAR
+        listaFavoritos.splice(sacarID, 1);
+        //Si ya lo saque --> cambio el texto de link
+        agregarAFav.innerHTML=`Agregar a Favoritos`
+        console.log(listaFavoritos)
+    }
+    //si no esta en mi lista
+   else {
+        //se agrega la canción actual
+        listaFavoritos.push(tvid);
+        //si ya lo agregué-->cambio texto 
+        agregarAFav.innerHTML = `Quitar de Favoritos`
 
-    
-
+    }
+    //guardo el array actualizado como string
+    let trackAStorage= JSON.stringify(listaFavoritos);
+    //Guardo el string en local storage
+    localStorage.setItem('fav', trackAStorage)
+    //chequeo*/
+    console.log(localStorage)
 })
+})
+.catch(function (error) {
+    console.log(error)
+})
+})
+   
+    
